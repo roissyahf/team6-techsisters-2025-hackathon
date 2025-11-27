052525ac41d04b4b42aa4ef0de331f002e3b6f5a
roissyahf <roissyahfk@gmail.com>
2025-11-28 06:13:17 +0700

    Adjust code structure in MODEL_API_TECHNICAL_COURSE

--- MODEL_API_TECHNICAL_COURSE/services/recommender_engine_course.py
+++ MODEL_API_TECHNICAL_COURSE/services/recommender_engine_course.py
@@ -4,7 +4,7 @@ import sys
 # Add project root so we can import MODEL_technical_questions
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 
-from MODEL_technical_questions.course_model_full import CourseRecommenderModel
+from MODEL_API_TECHNICAL_COURSE.services.course_model_full import CourseRecommenderModel
 
 course_model = CourseRecommenderModel()
 
